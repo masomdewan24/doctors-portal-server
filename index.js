c48f709e11ac3c8603140a903f90d47183d5f87b
masomdewan24 <masom24@gmail.com>
2022-11-22 23:20:02 +0600

    home 22_

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -22,25 +23,27 @@ async function run(){
     try{
         const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
+        const usersCollection = client.db('doctorsPortal').collection('users');
 
+        // Use Aggregate to query multiple collection and then merge data
         app.get('/appointmentOptions', async (req, res) => {
             const date = req.query.date;
             const query = {};
             const options = await appointmentOptionCollection.find(query).toArray();
+
             // get the bookings of the provided date
             const bookingQuery = { appointmentDate: date }
             const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
 
+            // code carefully :D
             options.forEach(option => {
                 const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
                 const bookedSlots = optionBooked.map(book => book.slot);
                 const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
                 option.slots = remainingSlots;
             })
-
-            res.send(options)
+            res.send(options);
         });
-
 
         app.get('/v2/appointmentOptions', async (req, res) => {
             const date = req.query.date;
@@ -87,13 +90,30 @@ async function run(){
             res.send(options);
         })
 
+        /***
+         * API Naming Convention 
+         * app.get('/bookings')
+         * app.get('/bookings/:id')
+         * app.post('/bookings')
+         * app.patch('/bookings/:id')
+         * app.delete('/bookings/:id')
+        */
+
+         app.get('/bookings', async(req, res) =>{
+            const email = req.query.email;
+            const query = {email:email};
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings)
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
             const query = {
                 appointmentDate: booking.appointmentDate,
                 email: booking.email,
-                treatment: booking.treatment 
+                treatment: booking.treatment,
+        
             }
 
             const alreadyBooked = await bookingsCollection.find(query).toArray();
@@ -105,19 +125,35 @@ async function run(){
 
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
+        });
+
+        app.get('/jwt', async(req, res) =>{
+            const email = req.query.email;
+            const query = {email: email}
+            const user = await usersCollection.findOne(query);
+           if(user) {
+            const token =jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
+            return res.send(accessTokens)
+           }
+           console.log(user);
+           res.status(403).send({accessTokens: ''})
+        })
+
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
         })
 
     }
-    finally{
+    finally {
 
     }
-
 }
 run().catch(console.log);
 
-
-app.get('/', async(req, res) =>{
-    res.send('doctors portal server is running')
+app.get('/', async (req, res) => {
+    res.send('doctors portal server is running');
 })
 
-app. listen(port, () => console.log(`doctors portel is run:${port}`))
+app.listen(port, () => console.log(`Doctors portal running on ${port}`))
